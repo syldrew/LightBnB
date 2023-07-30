906dcb38f8db5e8822333032ada74e946843552a
syldrew <syl_peke14@hotmail.com>
2023-07-30 14:27:03 -0500

    Update Date

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -14,9 +14,9 @@ VALUES
 
 INSERT INTO reservations (start_date, end_date, property_id, guest_id)
 VALUES 
-('2018-09-11', '2018-09-26', 1, 1),
+('2018-09-11', '2018-09-26', 2, 3),
 ('2019-01-04', '2019-02-01', 2, 2),
-('2021-10-01', '2021-10-14', 3, 3);
+('2023-10-01', '2023-10-14', 1, 3);
 
 
 INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message)
